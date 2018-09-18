aafa4bae0a0f0ad21e45d5cc074b7b01de01bedb
dunnywei <wei_don@yahoo.com>
2018-09-17 22:12:38 -0400

    (1:06)

--- SYSC_2003_part2.c
+++ SYSC_2003_part2.c
@@ -1,1 +1,17 @@
 link->https://www.youtube.com/watch?v=4gWCBDKHHW8&index=2&list=PL-k1KoUHjKI2u4ZfzewZy_UZ8MZrg1FDq
+
+->ORG=used to store a block of code at a certain location
+eg
+org $800
+   code
+   LDAA #7
+   
+   
+   
+   
+   0x800 
+   0x801
+   0x802
+   -(0:26) It means that you are able to store code segment at memory location 800 to start(0:39)
+   -(0:59) 0x800 to 0x801 is a.k.a memory map (1:06)
+   -
