3ba834d8dee4864e6f2de41dec983287927e7fb1
dunnywei <wei_don@yahoo.com>
2018-09-17 22:17:54 -0400

    (2:38)

--- SYSC_2003_part2.c
+++ SYSC_2003_part2.c
@@ -13,5 +13,11 @@ org $800
    0x801
    0x802
    -(0:26) It means that you are able to store code segment at memory location 800 to start(0:39)
-   -(0:59) 0x800 to 0x801 is a.k.a memory map (1:06)
-   -
+   -(0:59) 0x800 to 0x801 is a.k.a memory map (1:06)=memory w.r.t the location it stores (1:15)
+->Define a constant
+   db,dw
+   -(1:22)db for defining a byte(8 bits) and dw for defining a word (16 bits) (1:52)
+   eg
+   org $10
+   C db 5
+   -(2:38) starts at memory location 10 (2:38)
